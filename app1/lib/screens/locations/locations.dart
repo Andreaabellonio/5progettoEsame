68ef99a785d37fa6023ea6b98d8f733058375590
Andreaabellonio <51354427+Andreaabellonio@users.noreply.github.com>
2021-03-01 13:04:31 +0100

    gestione chiamata ajax e barcode
    Creazione di un'app per il test della chiamata ajax e del barcode
    FUNZIONANTE

--- app1/lib/screens/locations/locations.dart
+++ app1/lib/screens/locations/locations.dart
@@ -3,17 +3,14 @@ import '../../app.dart';
 import '../../models/location.dart';
 import '../../widgets/image_banner.dart';
 import './tile_overlay.dart';
-import 'package:translator/translator.dart';
 
 class Locations extends StatelessWidget {
   @override
   Widget build(BuildContext context) {    
     final locations = Location.fetchAll();
-    String variabile;
-    traduci("Title").then((value) => variabile=value);
     return Scaffold(
         appBar: AppBar(
-          title: Text(variabile),
+          title: Text("Title"),
         ),
         body: ListView.builder(
           //?crea una lista scrollable di widget
@@ -42,10 +39,4 @@ class Locations extends StatelessWidget {
       onTap: () => _onLocationTap(context, location.id),
     );
   }
-}
-
-Future<String> traduci(String input) async{
-  final translator = GoogleTranslator();  
-  var translation = await translator.translate(input, to: 'it');
-  return translation.toString();
 }
--- server/server.js
+++ server/server.js
@@ -1,27 +1,11 @@
 //#region ISTANZE MODULI E INIZIALIZZAZIONE
-//? VARIABILI GLOBALI
-const lingua = "it";
 
 //? CARICAMENTO DEI MODULI
 const express = require("express");
 const request = require("request");
 const mongo = require("mongodb");
 const bodyParser = require("body-parser");
-const fetch = require("node-fetch");
-var AWS = require("aws-sdk");
 
-AWSCredentialsProviderChain DefaultAWSCredentialsProviderChain = new AWSCredentialsProviderChain(
-                new SystemPropertiesCredentialsProvider(),
-                new EnvironmentVariableCredentialsProvider(),
-                new ProfileCredentialsProvider()
-        );
-
-        // Create an Amazon Translate client
-        AmazonTranslate translate = AmazonTranslateClient.builder()
-                .withCredentials(DefaultAWSCredentialsProviderChain)
-                .withRegion(region)
-                .build();
-                
 //? CREAZIONE DEL SERVER
 const app = express();
 app.listen(13377, function () {
@@ -40,47 +24,21 @@ app.use("/", bodyParser.urlencoded({ "extended": true }));
 //#endregion
 
 //#region GESTIONE RICHIESTE
-app.get("/prova", function (req, res, next) {
-    request('https://world.openfoodfacts.org/api/v0/product/8000500310427.json', async function (error, response, body) {
+app.post("/prova", function (req, res, next) {
+    console.log(req.body);
+    request('https://world.openfoodfacts.org/api/v0/product/' + req.body.codice.toString() + '.json', async function (error, response, body) {
         if (!error && response.statusCode == 200) {
             body = JSON.parse(body);
             let prodotto = new Object();
-            prodotto.nome = await asyncTranslate(lingua, body.product.generic_name);
-            prodotto.qta = body.product.quantity;
-            prodotto.tracce = await asyncTranslate(lingua, body.product.traces.split(":")[1]); //! gestire se non c'è ne sono
-            await res.send(JSON.stringify(prodotto));
+            prodotto.nome = body.product.product_name; //nome del prodotto
+            prodotto.qta = body.product.quantity; //quantità
+            prodotto.tracce = body.product.traces.split(":")[1]; //! gestire se non c'è ne sono
+            prodotto.urlImage = body.product.image_front_url; //immagine del prodotto
+            res.send(JSON.stringify(prodotto));
         }
-    });
-});
-app.get("/test", function (req, res, next) {
-    translate.translateText(params, function (err, data) {
-        if (err) console.log(err, err.stack);
-        else console.log(data['TranslatedText']);
     });
 });
 //#endregion
 
 //#region FUNZIONI AGGIUNTIVE
-//? FUNZIONE PER LA TRADUZIONE DEL TESTO
-async function traduci(target, text) {
-    let testo = await fetch("https://eu-central-1.console.aws.amazon.com/translate/api/translate", {
-        contentString: JSON.stringify({
-            "TargetLanguageCode": target,
-            "Text": text,
-            "SourceLanguageCode": "auto"
-        }),
-        headers: JSON.stringify({
-            "Content-Type": "application/x-amz-json-1.1",
-            "X-Amz-Target": "AWSShineFrontendService_20170701.TranslateText",
-            "X-Amz-User-Agent": "aws-sdk-js/2.306.0 promise"
-        }),
-        method: "POST",
-        operation: "translateText",
-        params: {},
-        path: "/",
-        region: "eu-central-1"
-    });
-    let tradotto = await testo;
-    return tradotto.TranslatedText;
-}
 //#endregion
