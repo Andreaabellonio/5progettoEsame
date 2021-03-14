dc9c74c4a00c48ffbccccfd148e81bc3b35f9f53
e.miclaus.1101@vallauri.edu <e.miclaus.1101@vallauri.edu>
2021-03-14 21:14:11 +0100

    aggiunta base per la dialogbox dell'aggiunta con barcode
    aggiunta base per la dialogbox dell'aggiunta con barcode ancora non funzionante

--- client/lib/screens/firstPage/firstPage.dart
+++ client/lib/screens/firstPage/firstPage.dart
@@ -1,40 +1,46 @@
-
 import 'package:flutter/material.dart';
 import 'package:client/screens/secondPage/secondPage.dart';
-
+import 'package:client/widgets/barcode_scan.dart';
+import 'package:client/widgets/dialog.dart';
 
 class FirstPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Routing App'),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: <Widget>[
-            Text(
-              'First Page',
-              style: TextStyle(fontSize: 50),
-            ),
-            RaisedButton(
-              child: Text('Go to second'),
-              onPressed: () {
-                // Pushing a route directly, WITHOUT using a named route
-                Navigator.of(context).push(
-                  // With MaterialPageRoute, you can pass data between pages,
-                  // but if you have a more complex app, you will quickly get lost.
-                  MaterialPageRoute(
-                    builder: (context) =>
-                        SecondPage(data: 'Hello there from the first page!'),
-                  ),
-                );
-              },
-            )
-          ],
+        appBar: AppBar(
+          title: Text('Routing App'),
         ),
-      ),
-    );
+        body: Center(
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: <Widget>[
+              Text(
+                'First Page',
+                style: TextStyle(fontSize: 50),
+              ),
+              RaisedButton(
+                child: Text('Go to second'),
+                onPressed: () {
+                  // Pushing a route directly, WITHOUT using a named route
+                  Navigator.of(context).push(
+                    // With MaterialPageRoute, you can pass data between pages,
+                    // but if you have a more complex app, you will quickly get lost.
+                    MaterialPageRoute(
+                      builder: (context) =>
+                          SecondPage(data: 'Hello there from the first page!'),
+                    ),
+                  );
+                },
+              ),
+            ],
+          ),
+        ),
+        // floatingActionButton:AddBarcode() ,
+        floatingActionButton: FloatingActionButton(
+          onPressed: () async {
+            final action =
+                await Dialogs.yesAbortDialog(context, 'My title', 'My Body');
+          },
+        ));
   }
 }
